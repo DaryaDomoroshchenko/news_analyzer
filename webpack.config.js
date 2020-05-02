eef1f18a8abae4c3870adf75d64f18c1cf7cc574
Darya Domoroshchenko <deydra.v@gmail.com>
2020-05-02 23:23:29 +0400

    header, footer, sections search and about-author added

--- webpack.config.js
+++ webpack.config.js
@@ -70,19 +70,19 @@ module.exports = {
       canPrint: true
     }),
     new HtmlWebpackPlugin({
-      inject: false,
+      inject: true,
       template: './src/pages/index/index.html',
       chunks: ['index'],
       filename: 'index.html'
     }),
     new HtmlWebpackPlugin({
-      inject: false,
+      inject: true,
       template: './src/pages/about/about.html',
       chunks: ['about'],
       filename: 'about.html'
     }),
     new HtmlWebpackPlugin({
-      inject: false,
+      inject: true,
       template: './src/pages/analytics/analytics.html',
       chunks: ['analytics'],
       filename: 'analytics.html'
